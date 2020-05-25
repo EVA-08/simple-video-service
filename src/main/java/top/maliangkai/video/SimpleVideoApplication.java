dc524006b922467b00f1d24aac168b180a053d54
maliangkai <maliangkai@itiger.com>
2020-05-25 19:12:12 +0800

    init

--- src/main/java/top/maliangkai/video/SimpleVideoApplication.java
+++ src/main/java/top/maliangkai/video/SimpleVideoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.content.commons.repository.Store;
 import org.springframework.content.rest.StoreRestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class SimpleVideoApplication {
@@ -12,7 +14,12 @@ public class SimpleVideoApplication {
 		SpringApplication.run(SimpleVideoApplication.class, args);
 	}
 
-	@StoreRestResource(path = "videos")
+	@StoreRestResource(path = "/videos")
 	public interface VideoStore extends Store<String> { }
 
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "hello";
+	}
 }
